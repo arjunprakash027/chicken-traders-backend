a4fc158bbdf2b8a486fa2287078109de22f8219b
arjun <arjunprakash027@gmail.com>
2023-04-08 14:18:51 +0530

    js test

---
+++ app/test.js
@@ -0,0 +1,27 @@
+//get all values in database
+const url = new URL('http://127.0.0.1:5001/getall');
+fetch(url).then(response => response.json()).then(data => console.log(data));
+
+//add new value to database
+const url_add = new URL('http://127.0.0.1:5001/add');
+const params_add = {item:'country chicken',date:'2020-01-01',weight:3,transtype:'purchase',remark:'testing with javascript'};
+url_add.search = new URLSearchParams(params_add).toString();
+fetch(url).then(response=>response.json()).then(data=>console.log(data));
+
+//update value in database
+const url_update = new URL('http://127.0.0.1:5001/update');
+const params_update = {item:'country chicken',date:'2020-01-01',weight:3,transtype:'sale',remark:'testing with javascript'};
+url_update.search = new URLSearchParams(params_update).toString();
+fetch(url).then(response=>response.json()).then(data=>console.log(data));
+
+//delete value in database
+const url_delete = new URL('http://127.0.0.1:5001/delete');
+const params_delete = {item:'country chicken'};
+url_delete.search = new URLSearchParams(params_delete).toString();
+fetch(url).then(response=>response.json()).then(data=>console.log(data));
+
+//fetch by value
+const url_fetch = new URL('http://127.0.0.1:5001/get');
+const params_fetch = {item:'country chicken'};
+url_fetch.search = new URLSearchParams(params_fetch).toString();
+fetch(url).then(response=>response.json()).then(data=>console.log(data));
--- app/test.py
+++ app/test.py
@@ -1,14 +1,13 @@
 import requests
-import json
 
 # Define the endpoint URL and the sentence to be sent
-endpoint = 'http://127.0.0.1:5001/update'
+endpoint = 'http://127.0.0.1:5001/getall'
 
 # Encode the sentence as a URL parameter
-url_param = {"item":"test","date":"2020-01-01","weight":"1","transtype":'purchase',"remark":"test"}
+url_param = {"item":"broiler","date":"2020-01-01","weight":"1","transtype":"sale","remark":"test"}
 
 # Send the request to the API endpoint with the URL parameter
 response = requests.get(endpoint, params=url_param)
 
 # Print the response from the API
-print(json.loads(response.text))
+print(response.text)
